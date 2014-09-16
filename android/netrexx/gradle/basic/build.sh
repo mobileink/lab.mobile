305b0fd5fd44b6c867bfaa9d3d14c1971e922155
Gregg Reynolds <dev@mobileink.com>
2014-09-16 08:39:32 -0500

    tweak basic/build.sh

--- android/netrexx/gradle/basic/build.sh
+++ android/netrexx/gradle/basic/build.sh
@@ -3,7 +3,8 @@
 # ./gradlew clean \
 # && ./gradlew compileDebugJava \
 
-./nrc src/main/java/org/example/myapp/*nrx && ./gradlew assembleDebug
+#  add nrc arg '-format' to get pretty-printed output
 
+./nrc src/main/nrx/org/example/myapp/*nrx && ./gradlew assembleDebug
 
 # /Applications/Android\ Studio.app/sdk/platform-tools/adb -d install -r build/outputs/apk/basic-debug.apk
